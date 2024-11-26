1343c9a52d98ad918cecb1f04b2b88fc22b027cf
FATHIMAN8 <84309918+FATHIMAN8@users.noreply.github.com>
2024-11-26 11:49:08 +0530

    Add files via upload

---
+++ BASIC PYTHON PROGRAMS/Positive or Negative number.py
@@ -0,0 +1,10 @@
+#check whether a number is positive , negative or 0
+num=float(input("Enter a number :"))
+if num>0:
+    print("positive number")
+elif num==0:
+    print("Zero")
+else:
+    print("negative number")
+
+    
